0e257b52d08f52b6a55671c7aa6249656aae63c5
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-05-20 13:32:23 +0100

    Trying to add an uglify button but the ugly class is being applied by default: not when ugly button gets clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 
 // grab the elements you want to manipulate
+var pageBody = document.querySelector('.portfolio-body');
+var uglifyButton = document.getElementById('ugly-button');
 
 
 // function to remove the portfolio-body class and apply the ugly class on click
+function uglify() {
+    pageBody.classList.remove('portfolio-body');
+    pageBody.classList.add('ugly');
+}
+
+// activate the button onclick
+// uglifyButton.onclick = uglify();
+uglifyButton.addEventListener('click', uglify());
