a8726a69e66b0dd4d664a5d4f1b5cc924cb2fc0b
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-05-20 16:40:22 +0100

    Started improving projects page

--- js/indexScripts.js
+++ js/indexScripts.js
@@ -2,22 +2,54 @@
 var arabicScriptElements = document.querySelectorAll('.arabic');
 
 
-// translate to Arabic and Persian
-function translateArabicAndPersian() {
-    element.classList.remove('arabic');
 
-    if(element.innerHTML = 'اللغة العربية') {
-        element.innerHTML = 'Arabic';
-    }
-    else {
-        element.innerHTML = 'Persian';
-    }
-
-
-}
 
 // activate the translation on hover
-arabicScriptElements.forEach(element => {
-    element.addEventListener('mouseover', translateArabicAndPersian);
-});
+// arabicScriptElements.forEach(element => {
+
+//     // translate to Arabic and Persian
+//     function translateArabicAndPersian() {
+//         element.classList.remove('arabic');
+
+//         if(element.textContent = 'اللغة العربية') {
+//             element.innerHTML = 'Arabic';
+//         }
+//         else if(element.textContent = 'فارسی') {
+//             element.innerHTML = 'Persian';
+//         }
+//     }
+
+//     element.addEventListener('mouseover', translateArabicAndPersian);
+// });
+
+// to-do: make this DRY
+function translateArabic() {
+    arabicScriptElements[0].classList.remove('arabic');
+    arabicScriptElements[0].innerHTML = 'Arabic';
+}
+
+function restoreArabicToArabicScript()
+{
+    arabicScriptElements[0].classList.add('arabic');
+    arabicScriptElements[0].innerHTML = 'اللغة العربية';
+}
+
+function translatePersian() {
+    arabicScriptElements[1].classList.remove('arabic');
+    arabicScriptElements[1].innerHTML = 'Persian';
+}
+
+function restorePersianToArabicScript()
+{
+    arabicScriptElements[1].classList.add('arabic');
+    arabicScriptElements[1].innerHTML = 'فارسی';
+}
+
+
+arabicScriptElements[0].addEventListener('mouseover', translateArabic);
+arabicScriptElements[0].addEventListener('mouseout', restoreArabicToArabicScript);
+arabicScriptElements[1].addEventListener('mouseover', translatePersian);
+arabicScriptElements[1].addEventListener('mouseout', restorePersianToArabicScript);
+
+
 
