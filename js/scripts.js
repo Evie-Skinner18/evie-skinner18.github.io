e3cd78939bf52d5bb1066badfcc735cb84844a75
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-05-20 15:38:00 +0100

    Added a beautify button to take user back to normal styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,37 @@
 
 // grab the elements you want to manipulate
 var pageBody = document.querySelector('.portfolio-body');
+var mainSection = document.querySelector('main');
+var jumbotrons = document.querySelectorAll('.jumbotron');
 var uglifyButton = document.getElementById('ugly-button');
+var beautifyButton = document.getElementById('beautify-button');
 
 
 // function to remove the portfolio-body class and apply the ugly class on click
 function uglify() {
     pageBody.classList.remove('portfolio-body');
-    pageBody.classList.add('ugly');
+    mainSection.classList.remove('portfolio-main');
+
+    jumbotrons.forEach(jumbotron => {
+        jumbotron.classList.remove('jumbotron')
+        jumbotron.classList.remove('description');
+    });
+
+    pageBody.classList.add('ugly');   
 }
 
+// restore to normal
 function beautify() {
-    pageBody.classList
+    pageBody.classList.remove('ugly');
+    pageBody.classList.add('portfolio-body');
+    mainSection.classList.add('portfolio-main');
+
+    jumbotrons.forEach(jumbotron => {
+        jumbotron.classList.add('jumbotron')
+        jumbotron.classList.add('description');
+    });
 }
-// activate the button onclick
+
+// activate the buttons onclick
 uglifyButton.onclick = uglify;
-//uglifyButton.addEventListener('click', uglify());
+beautifyButton.onclick = beautify;
